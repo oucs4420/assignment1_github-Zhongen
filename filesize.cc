18c93fbd4984e7f7ade03bd54b880fe162611120
Zhongen Li <luxzen.1208@gmail.com>
2022-01-14 16:21:10 -0500

    past all tests

--- filesize.cc
+++ filesize.cc
@@ -1,5 +1,11 @@
+/*
+Title: OS Assignment 1
+Author: Zhongen Li 
+*/
+
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
 // output format to use IDENTICALLY:
@@ -8,11 +14,37 @@ using namespace std;
 // input.3lines: 3
 // input.1line: 1
 
+
+// function prototype for reading user specified files and count the number of lines of each
+void readCountFile(string filename);
+
+
 int main( int argc, char* argv[] )
 {
     // just to get you started, this is how to refer to the arguments that were passed
-    for (int arg = 0; arg < argc; ++arg)
-            std::cout << "argv[" << arg << "]: " << argv[arg] << '\n' ;
-
+    for (int arg = 0; arg < argc; ++arg){
+            if (arg == 0)
+                cout << "program: " << argv[arg] << '\n' ;
+            else
+                readCountFile(argv[arg]);
+}   
     exit(0); // this means that the program executed correctly!
+}
+
+// function definition of the prototype
+void readCountFile(string filename)
+{
+    ifstream inFile(filename);
+    // if filed to open, return -1
+    if(!inFile.is_open())
+    {
+        cout << " " << filename << ": " << "-1" << endl;
+        return;
+    }
+    // otherwise return the number of lines in it
+    string t;
+    int lineCount = 0;
+    while(getline(inFile, t)) lineCount += 1;
+    cout << " " << filename <<  ": " << lineCount << endl;
+    inFile.close();
 }
